2326a5dbe5f0fca3a1b1deffc91ada18ff5a7de0
Gercius <gerciuswork@gmail.com>
2025-04-22 12:56:04 +0300

    Add ticket validation, formatting. Automatically update updatedAt fields.

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -12,21 +12,40 @@ export const validate = (validator) => [
 
 const registerValidator = [
     body("username")
-        .notEmpty().withMessage("Vartotojo vardas privalomas")
-        .isLength({ min: 6 }).withMessage("Vartotojo vardas turi būti bent 6 simbolių ilgio")
-        .isAlphanumeric().withMessage("Vartotojo vardas gali turėti tik raides ir skaičius"),
-        
+        .notEmpty()
+        .withMessage("Vartotojo vardas privalomas")
+        .isLength({ min: 6 })
+        .withMessage("Vartotojo vardas turi būti bent 6 simbolių ilgio")
+        .isAlphanumeric()
+        .withMessage("Vartotojo vardas gali turėti tik raides ir skaičius"),
+
     body("password")
-        .notEmpty().withMessage("Slaptažodis privalomas")
-        .isLength({ min: 8 }).withMessage("Slaptažodis turi būti bent 8 simbolių ilgio")
+        .notEmpty()
+        .withMessage("Slaptažodis privalomas")
+        .isLength({ min: 8 })
+        .withMessage("Slaptažodis turi būti bent 8 simbolių ilgio"),
 ];
 
 const loginValidator = [
-    body("username")
-        .notEmpty().withMessage("Vartotojo vardas privalomas"),
+    body("username").notEmpty().withMessage("Vartotojo vardas privalomas"),
+    body("password").notEmpty().withMessage("Slaptažodis privalomas"),
+];
 
-    body("password")
-        .notEmpty().withMessage("Slaptažodis privalomas")
+export const ticketValidator = [
+    body("title")
+        .notEmpty()
+        .withMessage("Pavadinimas privalomas")
+        .isLength({ max: 255 })
+        .withMessage("Pavadinimas turi būti ne ilgesnis nei 255 simboliai"),
+    body("description")
+        .notEmpty()
+        .withMessage("Aprašymas privalomas")
+        .isLength({ min: 10 })
+        .withMessage("Aprašymas turi būti bent 10 simbolių ilgio"),
+    body("status")
+        .optional()
+        .isIn(["NEW", "ANSWERED", "CLOSED"])
+        .withMessage("Statusas turi būti 'NEW', 'ANSWERED' arba 'CLOSED'"),
 ];
 
 export const authValidators = {
